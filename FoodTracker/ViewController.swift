b1f9f0f487ae126b989f10478991fc8fff137bdd
Tim Beals <tim.beals@gmail.com>
2016-12-04 13:45:15 -0800

    implement basic UI

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Tim Beals on 2016-12-04.
+//  Copyright © 2016 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
