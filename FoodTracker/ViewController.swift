35d7dbd3d8c1dbe34f93623237b4567b2e2d6b5a
Tim Beals <tim.beals@gmail.com>
2016-12-04 14:00:20 -0800

    connect UI to VC with textfield delegate

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        nameTextField.delegate = self
+        
+    }
+
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        mealNameLabel.text = textField.text
+    }
+    
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton)
+    {
+        mealNameLabel.text = "Default Text"
     }
 
 
